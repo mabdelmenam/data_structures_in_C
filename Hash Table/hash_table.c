3d4e5e3e716498630aeb51b2f44633658c7d0297
Mohamed Abdelmenam <mabdelmenam@gmail.com>
2025-07-10 16:24:31 -0400

    Planning

---
+++ Hash Table/hash_table.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//HashNode: to store a key-value pair
+//HashTable: to store the array of pointers to chains(linked lists)
+
+int main(){
+    return 0;
+}
