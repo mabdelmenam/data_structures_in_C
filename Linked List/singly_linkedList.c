9f1fd151885a627241d8790ec5e521fee96b0eef
Mohamed Abdelmenam <mabdelmenam@gmail.com>
2025-07-16 20:31:18 -0400

    Started Implementation of Singly Linked List

---
+++ Linked List/singly_linkedList.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// structure to create a linkedList
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+int main(){
+    return 0;
+}
